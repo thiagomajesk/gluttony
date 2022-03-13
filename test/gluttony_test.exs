7d319c8c88d523e0e0c91e0528cdca389dac0309
thiagomajesk <thiagomgoulart@gmail.com>
2022-03-13 09:17:14 -0300

    rename folder

--- test/gluttony_test.exs
+++ test/gluttony_test.exs
@@ -2,8 +2,8 @@ defmodule GluttonyTest do
   use ExUnit.Case
   doctest Gluttony
 
-  @rss_2_0 File.read!("test/samples/rss_2_0")
-  @atom_1_0 File.read!("test/samples/atom_1_0")
+  @rss_2_0 File.read!("test/fixtures/rss_2_0")
+  @atom_1_0 File.read!("test/fixtures/atom_1_0")
 
   test "detect rss 2.0" do
     assert Gluttony.detect(@rss_2_0) == :rss_2_0
