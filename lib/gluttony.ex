5fb39585e78c12d56ecfd85173c7c702f51fea3e
thiagomajesk <thiagomgoulart@gmail.com>
2022-03-13 00:00:12 -0300

    detect which type of feed is to be parsed

--- lib/gluttony.ex
+++ lib/gluttony.ex
@@ -3,16 +3,16 @@ defmodule Gluttony do
   Documentation for `Gluttony`.
   """
 
-  @doc """
-  Hello world.
+  def detect(xml) do
+    if parseable?(xml) do
+      cond do
+        Regex.match?(~r|<rss version="2.0"|i, xml) -> :rss_2_0
+        Regex.match?(~r|<feed xmlns="http://www.w3.org/2005/Atom"|i, xml) -> :atom_1_0
+      end
+    end
+  end
 
-  ## Examples
-
-      iex> Gluttony.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defp parseable?(xml) do
+    Regex.match?(~r|<\?xml version="1.0" encoding="utf-8"\?>|i, xml)
   end
 end
--- test/gluttony_test.exs
+++ test/gluttony_test.exs
@@ -2,7 +2,14 @@ defmodule GluttonyTest do
   use ExUnit.Case
   doctest Gluttony
 
-  test "greets the world" do
-    assert Gluttony.hello() == :world
+  @rss_2_0 File.read!("test/samples/rss_2_0")
+  @atom_1_0 File.read!("test/samples/atom_1_0")
+
+  test "detect rss 2.0" do
+    assert Gluttony.detect(@rss_2_0) == :rss_2_0
+  end
+
+  test "detect atom 1.0" do
+    assert Gluttony.detect(@atom_1_0) == :atom_1_0
   end
 end
